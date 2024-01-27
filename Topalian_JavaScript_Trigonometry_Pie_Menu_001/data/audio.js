b9ad6eea4ec1e3cecd8160b9574abcb97dc3c2c9
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-01-26 20:16:05 -0500

    Add files via upload

---
+++ Topalian_JavaScript_Trigonometry_Pie_Menu_001/data/audio.js
@@ -0,0 +1,25 @@
+// Dedicated to God the Father
+
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+
+// https://github.com/ChristopherTopalian
+
+// https://github.com/ChristopherAndrewTopalian
+
+// audio.js
+
+let audio = 
+[
+    {
+        name: 'sfx_blip_001',
+        id: 'sfx_blip_001',
+        src: 'sound/sfx_blip_001.mp4'
+    },
+
+    {
+        name: 'sfx_warp_001',
+        id: 'sfx_warp_001',
+        src: 'sound/sfx_warp_001.mp3'
+    }
+];
+
---
+++ Topalian_JavaScript_Trigonometry_Pie_Menu_001/data/logicGates.js
@@ -0,0 +1,53 @@
+// Dedicated to God the Father
+
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+
+// https://github.com/ChristopherTopalian
+
+// https://github.com/ChristopherAndrewTopalian
+
+// logicGates.js
+
+let logicGates = 
+[
+    {
+        name: 'Gate 1',
+        details: 'Details of Gate 1'
+    },
+
+    {
+        name: 'Gate 2',
+        details: 'Details  of Gate 2'
+    },
+
+    {
+        name: 'Gate 3',
+        details: 'Details  of Gate 3'
+    },
+
+    {
+        name: 'Gate 4',
+        details: 'Details  of Gate 4'
+    },
+
+    {
+        name: 'Gate 5',
+        details: 'Details  of Gate 5'
+    },
+
+    {
+        name: 'Gate 6',
+        details: 'Details  of Gate 6'
+    },
+
+    {
+        name: 'Gate 7',
+        details: 'Details  of Gate 7'
+    },
+
+    {
+        name: 'Gate 8',
+        details: 'Details  of Gate 8'
+    }
+];
+
